9ad4c2bf7b990a9b70216a48b2ffc1feca75492d
Christian <s215703@dtu.dk>
2021-10-26 15:58:42 +0200

    Kode so far

---
+++ src/TerningertilCDIO2.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class TerningertilCDIO2 {
+    private int værdi;
+    public void rul() {
+        Random rand = new Random();
+        værdi = rand.nextInt(6) + 1 + rand.nextInt(6) + 1;
+    }
+    public int getVærdi() {
+        return værdi;
+    }
+
+
+    public void TerningertilCDIO2() {
+
+        værdi = 1;
+
+
+    }
+}
