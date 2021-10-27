d09d1a2bfc73fc433a5f70e6299949d118ec6f14
Ali Hassan Tariq <am00ite21@gmail.com>
2021-10-27 10:45:38 +0200

    Rettet et par ting vedrørende dobbel taste en spillers tur

--- src/Goldmine.java
+++ src/Goldmine.java
@@ -29,57 +29,82 @@ public class Goldmine {
         switchObjekt.sprog(language);
 
 
-
         spillerObjekt.navn();
 
+  do {
 
-        while (p1score > 0 && p1score <= 3000 && p2score > 0 && p2score <= 3000) {
-
-
-            System.out.println("Tast a for at rulle");
-            Scanner p1 = new Scanner(System.in);
-            String type = p1.next();
-            if (type.equals("a")) {
-
-                do {
+      while (p1score > 0 && p1score <= 3000) {
 
 
-                    System.out.println("Spiller1");
-                    terningeObjekt.rul();
-                    System.out.println(terningeObjekt.getVærdi());
-                    switchObjekt.Felter(terningeObjekt.getVærdi());
-                    p1score = p1score + switchObjekt.point;
-                    System.out.println("P1 score:" + p1score);
+          System.out.println("Tast 'a' for at rulle");
+          Scanner p1 = new Scanner(System.in);
+          String type = p1.next();
+          if (type.equals("a")) {
+
+              do {
 
 
-                } while (terningeObjekt.getVærdi() == 10);
-            }  // else {
-               // System.out.println("Prøv igen tast a");
-            // }
+                  System.out.println("Spiller1");
+                  terningeObjekt.rul();
+                  System.out.println(terningeObjekt.getVærdi());
+                  switchObjekt.Felter(terningeObjekt.getVærdi());
+                  p1score = p1score + switchObjekt.point;
+                  System.out.println("P1 score:" + p1score);
 
 
+              } while (terningeObjekt.getVærdi() == 10);
+              if (terningeObjekt.getVærdi() != 10) {
+                  break;
+              }
+          }
+          if (!type.equals("a")) {
+              System.out.println("Prøv igen tast 'a'");
+              continue;
+          }
 
-            System.out.println("tast l for at rulle");
-            Scanner p2 = new Scanner(System.in);
-            String type1 = p2.next();
-            if (type1.equals("l")) {
-                do {
-                    System.out.println("Spiller2");
-                    terningeObjekt.rul();
-                    System.out.println(terningeObjekt.getVærdi());
-                    switchObjekt.Felter(terningeObjekt.getVærdi());
-                    p2score = p2score + switchObjekt.point;
-                    System.out.println("P2 score:" + p2score);
+      }
+      while (p2score > 0 && p2score <= 3000) {
+
+          System.out.println("tast 'l' for at rulle");
+          Scanner p2 = new Scanner(System.in);
+          String type1 = p2.next();
+          if (type1.equals("l")) {
+              do {
+                  System.out.println("Spiller2");
+                  terningeObjekt.rul();
+                  System.out.println(terningeObjekt.getVærdi());
+                  switchObjekt.Felter(terningeObjekt.getVærdi());
+                  p2score = p2score + switchObjekt.point;
+                  System.out.println("P2 score:" + p2score);
 
 
-                } while (terningeObjekt.getVærdi() == 10);
-            }  // else {
-              //  System.out.println("Prøv igen tast l");
-               // p2.next();
-           // }
+              } while (terningeObjekt.getVærdi() == 10);
+              if (terningeObjekt.getVærdi() != 10) {
+                  break;
+              }
+          }
+          if (!type1.equals("l")) {
+              System.out.println("Prøv igen tast 'l'");
+              continue;
+
+          }
+      }
+  } while (p1score < 3000 && p2score < 3000 && p1score > 0 && p2score > 0);
+
+  if (p1score > p2score) {
+      System.out.println("Tillykke spiller 1 har vundet" + spillerObjekt.Player1);
+  }
+  if (p1score < p2score) {
+      System.out.println("Tillyke spiller 2 har vundet" + spillerObjekt.Player2);
+  }
+  if (p1score == p2score) {
+      System.out.println("Den blev uafgjort");
+  }
+
+            }
 
         }
-        }
 
-    }
+
+
 
