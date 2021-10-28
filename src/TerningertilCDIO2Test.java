5544d20b65b1281ec7574a3b0b2182aa7ad64ec1
Seier K <seierk@gmail.com>
2021-10-28 21:00:58 +0200

    Test er færdig.

--- src/TerningertilCDIO2Test.java
+++ src/TerningertilCDIO2Test.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 
 import java.util.Random;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 public class TerningertilCDIO2Test {
 
@@ -18,7 +18,7 @@ public class TerningertilCDIO2Test {
         int dice1;
 
 
-        for (int i = 0; i < 10000; i++){
+        for (int i = 0; i < 10000; i++) {
             Random rand = new Random();
             dice1 = rand.nextInt(6) + 1;
 
@@ -44,21 +44,21 @@ public class TerningertilCDIO2Test {
 
         }
         System.out.println("Test over 10.000 slag:");
-        System.out.println("Antal 1'ere: " + counter1 );
-        System.out.println("Antal 2'ere: " + counter2 );
-        System.out.println("Antal 3'ere: " + counter3 );
-        System.out.println("Antal 4'ere: " + counter4 );
-        System.out.println("Antal 5'ere: " + counter5 );
-        System.out.println("Antal 6'ere: " + counter6 );
+        System.out.println("Antal 1'ere: " + counter1);
+        System.out.println("Antal 2'ere: " + counter2);
+        System.out.println("Antal 3'ere: " + counter3);
+        System.out.println("Antal 4'ere: " + counter4);
+        System.out.println("Antal 5'ere: " + counter5);
+        System.out.println("Antal 6'ere: " + counter6);
 
-        assertTrue("Testen er forkert", counter1 < 2166 && counter1 > 1166);
-        assertTrue("Testen er forkert", counter2 < 2166 && counter2 > 1166);
-        assertTrue("Testen er forkert", counter3 < 2166 && counter3 > 1166);
-        assertTrue("Testen er forkert", counter4 < 2166 && counter4 > 1166);
-        assertTrue("Testen er forkert", counter5 < 2166 && counter5 > 1166);
-        assertTrue("Testen er forkert", counter6 < 2166 && counter6 > 1166);
+        // tester med 5% afvigelse.
 
+        assertTrue("Testen er fejlet med 1'ere.", counter1 < 2166 && counter1 > 1166);
+        assertTrue("Testen er fejlet med 2'ere.", counter2 < 2166 && counter2 > 1166);
+        assertTrue("Testen er fejæet med 3'ere.", counter3 < 2166 && counter3 > 1166);
+        assertTrue("Testen er fejlet med 4'ere.", counter4 < 2166 && counter4 > 1166);
+        assertTrue("Testen er fejlet med 5'ere.", counter5 < 2166 && counter5 > 1166);
+        assertTrue("Testen er fejlet med 6'ere.", counter6 < 2166 && counter6 > 1166);
 
     }
-
 }
